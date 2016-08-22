27289c4eaa8adf563cdf1b7973aacd705f834e36
Ilija Vukotic <ivukotic@cern.ch>
2016-08-22 09:13:48 -0500

    fixes.

--- ATLASalertsservice/CheckClusterState.sh
+++ ATLASalertsservice/CheckClusterState.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 date
-d=$(date +%H)
+d=$(date +%H_%M)
 cd /home/ivukotic/workspace/AnalyticsNotebooks/ATLASalertsservice/
 /home/ivukotic/anaconda3/bin/jupyter nbconvert --execute CheckClusterState.ipynb  --output outputs/res_CheckClusterState_$d.ipynb --to notebook
