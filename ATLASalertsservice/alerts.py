1c0f118c2b3160fdb8167edbd0d79c06d67d520a
Ilija Vukotic <ivukotic@cern.ch>
2017-09-05 16:19:11 -0500

    manual commit

--- ATLASalertsservice/alerts.py
+++ ATLASalertsservice/alerts.py
@@ -1,12 +1,15 @@
 import requests, httplib2, json, time
 from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
 from apiclient import discovery
 import oauth2client
 from oauth2client import client
 from oauth2client import tools
 
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+import os
+
 from subprocess import Popen, PIPE
 
 
@@ -40,3 +43,47 @@ class alerts:
         print(msg.as_string())
         p.communicate(msg.as_string().encode('utf-8'))
 
+    def send_HTML_mail(self, test, to, body, subtitle="", images=[]):
+        msg = MIMEMultipart('related')
+        msg['Subject'] = test
+        msg['From'] = 'AAAS@mwt2.org'
+        msg['To'] = to
+        
+        msgAlternative = MIMEMultipart('alternative')
+        msg.attach(msgAlternative)
+
+        html = open("index.htm", "r").read()
+        image_template = open("image_template.htm", "r").read()
+        
+        html = html.replace('TheMainTitle',test)
+        html = html.replace('TheSubtitle',subtitle)
+        html = html.replace('MyBody', body)
+        
+        html = html.replace('TheImagePlaceholder1', image_template * int( (len(images)+1) / 2 ) )
+        html = html.replace('TheImagePlaceholder2', image_template * int(len(images)/2)    )
+        
+        for ind,i in enumerate(images):
+            #print("Adding image:", i)
+            html = html.replace('FigureTitle',i['Title'],2) #appears twice per figure
+            html = html.replace('FigureFilename',"cid:image"+str(ind),1)
+            html = html.replace('FigureDescription',i['Description'],1) 
+            link=''
+            if 'Link' in i:
+                link=i['Link']
+            html = html.replace('FigureLink',link,1)
+                
+            img_data = open(i['Filename'], 'rb').read()
+            image = MIMEImage(img_data, name=i['Filename'])
+            image.add_header('Content-ID', '<image'+str(ind)+'>')
+            msg.attach(image)
+            
+        # Record the MIME types of both parts - text/plain and text/html.
+        part1 = MIMEText(body, 'plain')
+        part2 = MIMEText(html, 'html')
+        
+        msgAlternative.attach(part1)
+        msgAlternative.attach(part2)
+        
+        p = Popen(["/usr/sbin/sendmail", "-t", "-oi", "-r AAAS@mwt2.org"], stdin=PIPE)
+        #print(msg.as_string())
+        p.communicate(msg.as_string().encode('utf-8'))
--- ATLASalertsservice/subscribers.py
+++ ATLASalertsservice/subscribers.py
@@ -6,9 +6,28 @@ import oauth2client
 from oauth2client import client
 from oauth2client import tools
 
-
+class user:
+    def __init__(self, email, name):
+        self.email = email
+        self.name  = name
+        self.link  = None
+        self.alerts= []
+        self.immediate_subscriptions = []
+        self.summary_subscriptions = []
+    def set_response_edit_link(self, link):
+        self.link  = link
+    def set_site(self, site_text):
+        self.sites  = [x.strip() for x in site_text.split(',')]
+    def set_queue(self, queue_text):
+        self.queues  = [x.strip() for x in queue_text.split(',')]
+    def add_immediate_subscription(self, subscription_response):
+        self.immediate_subscriptions.append(subscription_response)
+    def add_summary_subscription(self, subscription_response):
+        self.summary_subscriptions.append(subscription_response)
+    def to_string(self):
+        return "user name:" + self.name + "  email:" + self.email
+    
 class subscribers:
-    values=[]
     
     def __init__(self):
         SCOPE = ["https://spreadsheets.google.com/feeds"]
@@ -18,66 +37,57 @@ class subscribers:
         discoveryUrl = 'https://sheets.googleapis.com/$discovery/rest?version=v4'
         service = discovery.build('sheets', 'v4', http=http, discoveryServiceUrl=discoveryUrl)
 
-        spreadsheetId = '11kQlBCKJ_-NsQT6KYUIKo5hPRpkpM6UyRdJnxfVdJ4g'
-        rangeName = 'Form Responses'#!A2:E'
+        spreadsheetId = '1yGCC2flsRL1Kfw5sxI9tO1gD_fKCdHgKqo5A9KD3w78'
+        rangeName = 'Form responses'#!A2:E'
         result = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=rangeName).execute()
         self.values = result.get('values', [])
+        self.header = self.values[0]
+        self.datarows = self.values[1:]
+        self.load_users()
         return
     
-    def getTests(self):
+    def load_users(self):
+        """ a part of initialization, should not be called directly """
+        self.users = []
+        for nr, row in enumerate(self.datarows):
+            u = user(self.get_cell_value(nr,'Email address'), self.get_cell_value(nr,'Your name'))
+            u.set_response_edit_link(self.get_cell_value(nr,'ResponseEditLink'))
+            u.set_site(self.get_cell_value(nr,'Name of ATLAS site'))
+            u.set_queue(self.get_cell_value(nr,'Queue name(s)'))
+            for ncol, cell_value in enumerate(row):
+                if cell_value=="Immediate": u.add_immediate_subscription(self.header[ncol])
+                if cell_value=="Summary": u.add_summary_subscription(self.header[ncol])
+            self.users.append(u) 
+        
+    def get_cell_value(self, row_number, col_name):
+        """ returns value of a cell in the spreadsheet """
+        col_index = self.header.index(col_name)
+        return self.datarows[row_number][col_index]
+    
+    def get_all_users(self):
+        """ returns a list with all the users """
+        return self.users
+    
+    def get_user(self, email):
+        """ returns a user having a certain email """
+        for u in self.users:
+            if u.email==email:
+                return u
+        return
+    
+    def get_immediate_subscribers(self, alert_name):
+        """ returns a list of all subscribers to the immediate alert. """
         ret=[]
-        testColumns=[4,6,7,8,10,11]
-        for row in self.values:
-            if row[0]=='Timestamp' : continue
-            for col,val in enumerate(row):
-                if col not in testColumns: continue
-                if val is None: continue
-                if len(val)<1: continue
-                tests = val.split(',')
-                for test in tests:
-                    test=test.strip()
-                    if test not in ret:
-                        ret.append(test)
+        for u in self.users:
+            if alert_name in u.immediate_subscriptions:
+                ret.append(u)
         return ret
 
-    def getAllUserBasicInfo(self):
-        ret={}
-        for row in self.values:
-            if row[0]=='Timestamp' : continue
-            email=row[1]
-            user={'email':email, 'name':row[3], 'link':row[2], 'alerts':[]}
-            ret[email]=user 
-        return ret
-
-    def getSubscribers(self, testname):
+    def get_summary_subscribers(self, alert_name):
+        """ returns a list of all subscribers to the summary alert. """
         ret=[]
-        testColumns=[4,6,7,8,10,11]
-        for row in self.values:
-            if row[0]=='Timestamp' : continue
-            for col,val in enumerate(row):
-                if col not in testColumns: continue
-                if val is None: continue
-                if len(val)<1: continue
-                tests = val.split(',')
-                for test in tests:
-                    test=test.strip()
-                    if test==testname:
-                        ret.append([row[3],row[1],row[2]]) #name, email, link
+        for u in self.users:
+            if alert_name in u.summary_subscriptions:
+                ret.append(u)
         return ret
-
-    def getSubscribers_withSiteName(self,testname):
-        ret=[]
-        testColumns=[6,7,8,10]
-        siteForTest={6:5,7:5,8:5,10:9}
-        for row in self.values:
-            if row[0]=='Timestamp' : continue
-            for col,val in enumerate(row):
-                if col not in testColumns: continue
-                if val is None: continue
-                if len(val)<1: continue
-                tests = val.split(',')
-                for test in tests:
-                    test=test.strip()
-                    if test==testname:
-                        ret.append([row[3],row[1],row[2],row[siteForTest[col]]]) #name, email, link, site name
-        return ret
+    
